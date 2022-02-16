76e60211a043cb334047eb31cfc5917793f89c72
Edgar <edgar.morillo@sofka.com.co>
2022-02-16 16:40:02 -0500

    subida de todos los archivos

---
+++ src/main/java/com/books/integrate/spring/react/SpringBootReactMysqlApplication.java
@@ -0,0 +1,14 @@
+package com.books.integrate.spring.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactMysqlApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootReactMysqlApplication.class, args);
+	}
+
+}
