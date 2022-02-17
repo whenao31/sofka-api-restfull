b8d825fab047041984c7895d4ff8c8e375a3b152
wil <whenao86@gmail.com>
2022-02-17 16:44:55 -0500

    Crear endpoint que elimina por titulo.
    La url del endpoint es /api/tutorials/title/{title}.
    Al ingresar la url para acceder a este endpoint, si {title}
    contiene espacios(" "), estos deben ser reemplazados por el
    caracter "+".

--- src/main/java/com/books/integrate/spring/react/controller/TutorialController.java
+++ src/main/java/com/books/integrate/spring/react/controller/TutorialController.java
@@ -1,25 +1,19 @@
 package com.books.integrate.spring.react.controller;
 
-import java.util.*;
-
 import com.books.integrate.spring.react.model.Tutorial;
+import com.books.integrate.spring.react.repository.TutorialRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.books.integrate.spring.react.repository.TutorialRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
 
+@Slf4j
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
@@ -93,6 +87,19 @@ public class TutorialController {
 			tutorialRepository.deleteById(id);
 				return new ResponseEntity<>("Tutorials DELETE!! ",HttpStatus.NO_CONTENT);
 			} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+		}
+	}
+
+	@DeleteMapping("/tutorials/title/{title}")
+	public ResponseEntity<String> deleteByTitle(@PathVariable("title") String title) {
+		log.info(title);
+		try {
+			List<Tutorial> tutorials = tutorialRepository.findByTitleContaining(title.replace('+',' '));
+			tutorialRepository.deleteById(tutorials.iterator().next().getId());
+			return new ResponseEntity<>("Tutorials DELETE!! ",HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			log.info(e.getMessage());
 			return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
 		}
 	}
